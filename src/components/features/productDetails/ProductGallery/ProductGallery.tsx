f25f306abc837528343bdb1e18b95ca6ac3a0baf
MostafaMemari <m.memari711@gmail.com>
2025-08-23 20:06:49 +0330

    refactor: integrate embla-carousel-react for improved image gallery functionality; replace GalleryModal with GalleryDialog and CarouselWithThumbs components for enhanced user experience

--- src/components/features/productDetails/ProductGallery/ProductGallery.tsx
+++ src/components/features/productDetails/ProductGallery/ProductGallery.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
 import MainImage from './MainImage';
 import GalleryImage from './GalleryImage';
-import GalleryModal from './GalleryModal';
+import GalleryDialog from './GalleryDialog';
 import { ProductGalleriesType } from '@/types/productGalleriesType';
+import { useBoolean } from '@/hooks/use-boolean';
 
 interface ProductGalleryProps {
   product: ProductGalleriesType;
 }
 
 export default function ProductGallery({ product }: ProductGalleryProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const galleryImageDialogController = useBoolean();
 
   const { selectedVariant } = useSelector((state: RootState) => state.product);
 
@@ -28,19 +28,19 @@ export default function ProductGallery({ product }: ProductGalleryProps) {
   return (
     <>
       <div className="space-y-4">
-        <div onClick={() => setIsModalOpen(true)} className="cursor-pointer">
+        <div onClick={galleryImageDialogController.onTrue} className="cursor-pointer">
           {mainImage && <MainImage src={mainImage.fileUrl} alt={mainImage.title ?? product.name} />}
         </div>
 
         {product.galleryImages?.length > 0 && (
           <div className="flex items-center justify-center gap-x-2">
             {displayedImages.map((image, index) => (
-              <div key={index} onClick={() => setIsModalOpen(true)}>
+              <div key={index} onClick={galleryImageDialogController.onTrue}>
                 <GalleryImage src={image.fileUrl} alt={image.title ?? product.name} isBlurred={false} />
               </div>
             ))}
             {blurredImage && (
-              <div onClick={() => setIsModalOpen(true)} className="shrink-0">
+              <div onClick={galleryImageDialogController.onTrue} className="shrink-0">
                 <GalleryImage src={blurredImage.fileUrl} alt={blurredImage.title ?? product.name} isBlurred={true} />
               </div>
             )}
@@ -48,7 +48,7 @@ export default function ProductGallery({ product }: ProductGalleryProps) {
         )}
       </div>
 
-      <GalleryModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} images={modalImages} title={product.name} />
+      <GalleryDialog dialogController={galleryImageDialogController} images={modalImages} title={product.name} />
     </>
   );
 }
