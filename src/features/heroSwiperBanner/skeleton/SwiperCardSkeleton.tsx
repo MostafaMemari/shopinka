c4a00764ef0c7a15577dd46f71ebff77b6a49d36
MostafaMemari <m.memari711@gmail.com>
2025-12-23 12:21:33 +0330

    fix: adjust margin in SwiperCardSkeleton for better layout consistency

--- src/features/heroSwiperBanner/skeleton/SwiperCardSkeleton.tsx
+++ src/features/heroSwiperBanner/skeleton/SwiperCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function SwiperCardSkeleton() {
   return (
-    <Card className="relative border p-0 ml-3 overflow-hidden">
+    <Card className="relative border p-0 mx-1.5 overflow-hidden">
       <CardContent className="flex aspect-[3/4] flex-col items-center p-2 gap-2">
         <Skeleton className="h-4 w-16 rounded-md" />
 
