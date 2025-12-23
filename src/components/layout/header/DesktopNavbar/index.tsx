851bf64482d17971632700219483085a20010d91
MostafaMemari <m.memari711@gmail.com>
2025-12-23 11:16:40 +0330

    feat: refactor header components; streamline layout structure and improve container usage in DesktopNavbar and Header

--- src/components/layout/header/DesktopNavbar/index.tsx
+++ src/components/layout/header/DesktopNavbar/index.tsx
@@ -34,8 +34,8 @@ const DesktopNavbar = ({ categories }: DesktopNavbarProps) => {
     <nav
       className={cn('border-b transition-transform duration-700 px-4 bg-white shadow', isVisible ? 'translate-y-0' : '-translate-y-full')}
     >
-      <div className="container mx-auto max-w-[1640px] flex items-center gap-x-2">
-        <div className="flex justify-between items-center w-full">
+      <div className="mx-auto max-w-[1640px] flex items-center gap-x-2">
+        <div className="container flex justify-between items-center w-full">
           <div className="flex gap-x-2">
             <div className="flex items-center gap-x-2">
               <NavigationMenuDesktop categories={categories || []} />
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,4 +1,3 @@
-// Header.tsx
 import DesktopLogo from '../../common/Logo/DesktopLogo';
 import ProfileDropdown from './ProfileDropdown';
 import SearchBarBase from './Search/SearchBar';
@@ -19,28 +18,26 @@ async function Header() {
   const headerHeight = 92;
 
   return (
-    <>
-      <header className="hidden md:block">
-        <div className="fixed left-0 right-0 top-0 z-30 bg-white" style={{ height: headerHeight }}>
-          <div className="hidden md:block">
-            <div className="container flex max-w-[1640px] items-center justify-between gap-x-4 py-4">
-              <div className="flex items-center gap-x-6">
-                <DesktopLogo />
-                <SearchBarBase />
-              </div>
-              <div className="flex items-center gap-x-3">
-                <ProfileDropdown />
-                <div className="h-6 w-px bg-border" />
-                <BasketDropdown />
-              </div>
+    <header className="hidden md:block container">
+      <div className="fixed left-0 right-0 top-0 z-30 bg-white" style={{ height: headerHeight }}>
+        <div className="hidden md:block container">
+          <div className="container flex max-w-[1640px] items-center justify-between gap-x-4 py-4">
+            <div className="flex items-center gap-x-6">
+              <DesktopLogo />
+              <SearchBarBase />
+            </div>
+            <div className="flex items-center gap-x-3">
+              <ProfileDropdown />
+              <div className="h-6 w-px bg-border" />
+              <BasketDropdown />
             </div>
           </div>
         </div>
-        <div className={cn('hidden md:block fixed left-0 right-0 z-20')} style={{ top: headerHeight }}>
-          <DesktopNavbar categories={categories} />
-        </div>
-      </header>
-    </>
+      </div>
+      <div className={cn('hidden md:block fixed left-0 right-0 z-20')} style={{ top: headerHeight }}>
+        <DesktopNavbar categories={categories} />
+      </div>
+    </header>
   );
 }
 
