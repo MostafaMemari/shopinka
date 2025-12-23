8a771820644ea144802e8fbfd6e5d8e3ac2495e6
MostafaMemari <m.memari711@gmail.com>
2025-12-23 15:44:50 +0330

    feat: enhance CartPageItem component; implement custom sticker handling and integrate CustomStickerDialog for improved user interaction

--- src/features/cart/components/views/CartPageView/CartPageItem.tsx
+++ src/features/cart/components/views/CartPageView/CartPageItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Package } from 'lucide-react';
@@ -9,6 +9,7 @@ import CartControls from '@/features/cart/components/CartControls';
 
 import ProductCartPrice from '../../ProductCartPrice';
 import CartItemCustomSticker from '../../CartItemCustomSticker';
+import CustomStickerDialog from '../CartBasket/CustomStickerDialog';
 
 interface CartPageItemProps {
   cartItem: CartItemState;
@@ -16,50 +17,79 @@ interface CartPageItemProps {
 }
 
 function CartPageItem({ cartItem, isLast = false }: CartPageItemProps) {
+  const [open, setOpen] = useState(false);
+
   const attributes = cartItem.type === 'VARIABLE' && cartItem.attributeValues ? cartItem.attributeValues : [];
   const productUrl = `/product/${cartItem.slug}`;
+  const isCustomSticker = cartItem.type === 'CUSTOM_STICKER' || cartItem.customStickerValues !== null;
 
   return (
-    <div className={`py-4 px-2 sm:px-4 flex gap-4 items-center border-b border-gray-200 ${isLast ? 'border-b-0' : ''}`}>
-      <div className="flex-shrink-0">
-        <Link href={productUrl}>
-          <Image
-            alt={cartItem.title}
-            src={cartItem.thumbnail}
-            width={96}
-            height={96}
-            loading="lazy"
-            className="w-24 h-24 rounded-md object-cover border"
-          />
-        </Link>
-      </div>
-
-      <div className="flex-1 flex flex-col justify-between space-y-2">
-        <Link href={productUrl} className="text-sm sm:text-base font-medium text-gray-900 hover:text-blue-600 line-clamp-2">
-          {cartItem.title}
-        </Link>
-
-        <div className="flex items-center mt-1 gap-3">
-          <div className="flex items-center gap-1 text-xs text-gray-500 ">
-            <Package className="w-4 h-4 text-gray-400" />
-            <span>موجود در انبار</span>
-          </div>
-
-          <div className="text-xs text-gray-500">
-            <CartItemAttributes type={cartItem.type} attributes={attributes} />
-            {cartItem.customStickerValues && <CartItemCustomSticker customStickerValues={cartItem.customStickerValues} />}
-          </div>
+    <>
+      <div className={`py-4 px-2 sm:px-4 flex gap-4 items-center border-b border-gray-200 ${isLast ? 'border-b-0' : ''}`}>
+        <div className="flex-shrink-0">
+          {isCustomSticker ? (
+            <button type="button" onClick={() => setOpen(true)} className="focus:outline-none cursor-pointer">
+              <Image
+                alt={cartItem.title}
+                src={cartItem.thumbnail}
+                width={96}
+                height={96}
+                loading="lazy"
+                className="w-24 h-24 rounded-md object-cover border"
+              />
+            </button>
+          ) : (
+            <Link href={productUrl}>
+              <Image
+                alt={cartItem.title}
+                src={cartItem.thumbnail}
+                width={96}
+                height={96}
+                loading="lazy"
+                className="w-24 h-24 rounded-md object-cover border"
+              />
+            </Link>
+          )}
         </div>
 
-        <div className="flex items-center justify-between mt-3">
-          <div className="flex items-center gap-3">
-            <CartControls product={cartItem} className="w-22 lg:w-30 h-8 lg:h-10" />
-            <span className="text-xs hidden lg:block text-gray-600">تعداد: {cartItem.count}</span>
+        <div className="flex-1 flex flex-col justify-between space-y-2">
+          {isCustomSticker ? (
+            <button
+              onClick={() => setOpen(true)}
+              className="cursor-pointer text-sm text-start sm:text-base font-medium text-gray-900 hover:text-blue-600 line-clamp-2"
+            >
+              {cartItem.title}
+            </button>
+          ) : (
+            <Link href={productUrl} className="text-sm sm:text-base font-medium text-gray-900 hover:text-blue-600 line-clamp-2">
+              {cartItem.title}
+            </Link>
+          )}
+
+          <div className="flex items-center mt-1 gap-3">
+            <div className="flex items-center gap-1 text-xs text-gray-500 ">
+              <Package className="w-4 h-4 text-gray-400" />
+              <span>موجود در انبار</span>
+            </div>
+
+            <div className="text-xs text-gray-500">
+              <CartItemAttributes type={cartItem.type} attributes={attributes} />
+              {cartItem.customStickerValues && <CartItemCustomSticker customStickerValues={cartItem.customStickerValues} />}
+            </div>
           </div>
-          <ProductCartPrice salePrice={cartItem.salePrice * cartItem.count} basePrice={cartItem.basePrice * cartItem.count} />
+
+          <div className="flex items-center justify-between mt-3">
+            <div className="flex items-center gap-3">
+              <CartControls product={cartItem} className="w-22 lg:w-30 h-8 lg:h-10" />
+              <span className="text-xs hidden lg:block text-gray-600">تعداد: {cartItem.count}</span>
+            </div>
+            <ProductCartPrice salePrice={cartItem.salePrice * cartItem.count} basePrice={cartItem.basePrice * cartItem.count} />
+          </div>
         </div>
       </div>
-    </div>
+
+      {isCustomSticker && <CustomStickerDialog open={open} onOpenChange={setOpen} customStickerValues={cartItem.customStickerValues} />}
+    </>
   );
 }
 
