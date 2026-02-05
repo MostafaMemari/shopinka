d14fcaeba3e4da5c885f6451cd4df270b390c515
MostafaMemari <m.memari711@gmail.com>
2026-02-05 12:18:31 +0330

    feat: add docker file and build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   // experimental: {
   //   reactCompiler: true,
   // },
+  output: "standalone",
   images: {
     domains: ['s3.shopinka.ir', 'api.domingo.ir', 'car.vimascript.ir'],
   },
